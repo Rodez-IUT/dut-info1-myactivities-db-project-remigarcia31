bc2b8616421284f08b31ee6242424655c2bc04c0
remigarcia31 <remi.garcia@iut-rodez.fr>
2020-04-01 15:18:46 +0200

    fix #2 requete P1-5.1 sur activity

--- P1-5-1-select-all-activities.sql
+++ P1-5-1-select-all-activities.sql
@@ -1,6 +1,6 @@
 SELECT title, description, username
-FROM activity
-LEFT OUTER JOIN "user" usr
-ON usr.id = activity.owner_id
-WHERE creation_date > '09-01-2019'
-ORDER BY title, username ASC
+FROM activity 
+LEFT JOIN "user"
+ON owner_id = "user".id
+WHERE creation_date > '2019-09-01'
+ORDER BY title, username;
